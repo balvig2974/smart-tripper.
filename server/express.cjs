82d6b07b32d65ad951ed62e3004429da58a8d14e
balvig2974 <bala29vignesh@gmail.com>
2024-10-25 22:10:28 +0530

    Your commit message here

--- server/express.cjs
+++ server/express.cjs
@@ -5,7 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT||5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -203,7 +203,7 @@ app.post('/api/dates', async (req, res) => {
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(PORT, () => {
+    app.listen(PORT,'0.0.0.0',() => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   } catch (error) {
